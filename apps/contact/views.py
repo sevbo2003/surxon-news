f6da63a11de289630af9500f932be9b5c67c32c6
Abdusamad <sevbofx@gmail.com>
2022-10-06 00:30:29 +0500

    contact `view` created

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from apps.contact.models import Contact
+from apps.contact.forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'contact.html', {'form': form})
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
