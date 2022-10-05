9345d86960ab3709ecdc4c4fb3cee374e5c1be9c
Abdusamad <sevbofx@gmail.com>
2022-10-05 23:46:44 +0500

    `category` url implemented

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.blog.views import home, post_detail
+from apps.blog.views import home, post_detail, posts_by_category
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
+    path('category/<slug:category>/', posts_by_category, name='posts_by_category'),
 ]
