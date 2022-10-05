2c4834fd021c04e783fcf1df1580504d4955f913
Abdusamad <sevbofx@gmail.com>
2022-10-06 00:26:13 +0500

    `contact admin` implemented

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from apps.contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'phone_number', 'email', 'created_at']
+    list_filter = ['full_name', 'phone_number', 'email', 'created_at']
+    search_fields = ['full_name', 'phone_number', 'email', 'created_at']
+    list_per_page = 50
+
+    class Meta:
+        model = Contact
