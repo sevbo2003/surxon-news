f002055a27bce819994edc86a397694531620163
Abdusamad <sevbofx@gmail.com>
2022-10-09 12:43:18 +0500

    `Fucked` by bug

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -17,16 +17,8 @@ def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, created_at__year=year, created_at__month=month, created_at__day=day)
     post.views += 1
     post.save()
-    next_post = Post.objects.filter(created_at__gt=post.created_at).order_by('created_at').first()
-    previous_post = Post.objects.filter(created_at__lt=post.created_at).order_by('-created_at').first()
-    same_posts = Post.objects.filter(category=post.category).exclude(id=post.id).order_by('-created_at')
-    same_owner_posts = Post.objects.filter(author=post.author).exclude(id=post.id).order_by('-created_at')
     context = {
         'post': post,
-        'next_post': next_post,
-        'previous_post': previous_post,
-        'same_posts': same_posts,
-        'same_owner_posts': same_owner_posts,
     }
     return render(request, 'post_detail.html', context)
 
